1184c584d11e79ae3a6fde11186578152fee468d
Herberticleber Carvalho Valadares <herbert.valadares@ssp.df.gov.br>
2023-04-28 17:06:35 -0300

    comentando pra buildar

--- src/components/CustomInput/index.tsx
+++ src/components/CustomInput/index.tsx
@@ -1,30 +1,11 @@
-// import { useRef } from "react"
+import { useRef } from 'react'
 
-// export function CustomInput({
-//     title,
-//     placeholder,
-// }: {
-//     title: string
-//     placeholder?: string
-// }) {
-
-//     // export function BotaoCu(propriedades: any) {
-//     return (
-//         <>
-//             <div>
-//                 <label>{title}</label>
-//             </div>
-//             <div>
-//             <input
-//                         onChange={(e) => setPokemonBuscado(e.target.value)}
-//                         placeholder="Nome do Pokémon"
-//                         value={pokemonBuscado}
-//                     />
-//                 <input ref={inputRef} placeholder={placeholder} />
-//             </div>
-//             <div>
-//                 <button onClick={pesquisarPokemon}>PESQUISAR</button>
-//             </div>
-//         </>
-//     )
-// }
+export function CustomInput({ title }: { title: string }) {
+    return (
+        <>
+            <div>
+                <label>{title}</label>
+            </div>
+        </>
+    )
+}
--- src/pages/fortnite/index.tsx
+++ src/pages/fortnite/index.tsx
@@ -1,23 +1,8 @@
-import { CustomInput } from '@/components/BotaoCu'
 import { Inter } from 'next/font/google'
 // import style from './style.scss'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Fortnite() {
-    return (
-        <div>
-            <h1 className={inter.className}>Fortnite</h1>
-            <CustomInput
-                title="Nome:"
-                placeholder="Digite seu nome"
-                type="text"
-            ></CustomInput>
-            <CustomInput
-                title="Celular:"
-                placeholder="(99) 9 9999-9999"
-                type="phone"
-            ></CustomInput>
-        </div>
-    )
+    return <div>Hello</div>
 }
