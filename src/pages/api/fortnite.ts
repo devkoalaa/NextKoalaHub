5bb8af996bdedb3b13bfcfbc99eb61523faefd13
Herberticleber Carvalho Valadares <herbert.valadares@ssp.df.gov.br>
2023-05-15 14:14:21 -0300

    update

--- src/pages/api/fortnite.ts
+++ src/pages/api/fortnite.ts
@@ -2,10 +2,10 @@ import FortPlayerInterface from '@/interfaces/FortPlayerInterface'
 
 const URL_API = 'https://fortnite-api.com/v2/stats/br/v2/?'
 
-export async function FortGetSeason(player: string) {
+export async function FortGetAll(player: string) {
    try {
       const data: FortPlayerInterface = await fetch(
-         URL_API + new URLSearchParams({ name: player, timeWindow: 'season' }),
+         URL_API + new URLSearchParams({ name: player }),
          {
             headers: { Authorization: '3a924cb0-69a8-43d5-aa6d-69fc12b40991' },
          }
@@ -23,10 +23,10 @@ export async function FortGetSeason(player: string) {
    }
 }
 
-export async function FortGetAll(player: string) {
+export async function FortGetSeason(player: string) {
    try {
       const data: FortPlayerInterface = await fetch(
-         URL_API + new URLSearchParams({ name: player }),
+         URL_API + new URLSearchParams({ name: player, timeWindow: 'season' }),
          {
             headers: { Authorization: '3a924cb0-69a8-43d5-aa6d-69fc12b40991' },
          }
